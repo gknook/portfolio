a30a572a03d306fecbb2da12ff3b018388460746
gknook <gknook@gmail.com>
2021-06-26 12:55:37 +0200

    create case study noise monitor

---
+++ src/pages/NoiseMeter/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="max-w-screen-md px-4 mx-auto w-full">
+    <div id="introduction" class="mb-8">
+      <hgroup class="mb-4">
+        <h2 class="text-indigo-50 text-sm uppercase font-bold mb-4">
+          Case study
+        </h2>
+        <h1 class="text-indigo-50 sm:text-3xl text-xl font-bold mb-2">
+          Noise meter
+        </h1>
+        <div class="text-indigo-50 sm:text-lg text-md flex items-end">
+          <span class="mb-0">2017 - </span><img src="/src/assets/company_logos/Mimi.png" class="h-10"/>
+        </div>
+      </hgroup>
+
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Situation</h4> 
+      <p class="text-indigo-50 mb-8">
+        Our hearing test was already doing very well, around a million people had already tested their hearing with our app.
+      </p>
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Metric</h4> 
+      <p class="text-indigo-50 mb-8">The main metric was completed tests. They're worth measuring because they equate to the core value for the user, but also because they generate data to further improve our algorithms.</p>
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Problem</h4> 
+      <p class="text-indigo-50 mb-8">
+       Despite the large number of downloads, not everyone started a hearing test. Analyzing the funnel, I discovered that the biggest bottleneck had become the noise meter. This was a feature that we built in to show people it was noisy in their environment.
+      </p>
+    </div>
+    <Hypotheses />
+    <div class="absolute left-0 bottom-0 w-80 h-80 lg:block hidden">
+      <ProfilePic />
+    </div>
+  </div>
+  
+</template>
+<script>
+// import { gsap } from "gsap";
+import Hypotheses from "/src/components/hypotheses/Hypotheses.vue";
+import ProfilePic from "/src/assets/svg/ProfilePic.vue";
+
+export default {
+  components: { Hypotheses, ProfilePic },
+  data() {
+    return {
+      }
+  }
+};
+</script>
+<style>
+.conv-option:hover div {
+  background-color: #fff;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Introduction from "/src/pages/Introduction/index.vue";
+import NoiseMeter from "/src/pages/NoiseMeter/index.vue";
 
-const routes = [{ path: "/", name: "home", component: Introduction }];
+const routes = [{ path: "/", name: "home", component: Introduction }, { path: "/case-study/noise-meter/", name: "noise meter", component: NoiseMeter }];
 
 const router = createRouter({
   history: createWebHistory(),
