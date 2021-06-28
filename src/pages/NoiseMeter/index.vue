a16bb8c37419fc4e637ac0819ff408657f659224
gknook <gknook@gmail.com>
2021-06-28 22:05:00 +0200

    built the first case study

--- src/pages/NoiseMeter/index.vue
+++ src/pages/NoiseMeter/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="max-w-screen-md px-4 mx-auto w-full">
-    <div id="introduction" class="mb-8">
+  <div class="flex px-4 mx-auto w-full">
+    <div id="introduction" class="mb-8 lg:w-1/2 px-12">
       <hgroup class="mb-4">
         <h2 class="text-indigo-50 text-sm uppercase font-bold mb-4">
           Case study
@@ -9,38 +9,62 @@
           Noise meter
         </h1>
         <div class="text-indigo-50 sm:text-lg text-md flex items-end">
-          <span class="mb-0">2017 - </span><img src="/src/assets/company_logos/Mimi.png" class="h-10"/>
+          <span class="mb-0">2017 - </span>
+          <div class="mx-2 bg-gray-900 rounded-lg">
+            <img src="/src/assets/company_logos/Mimi.png" class="h-10" />
+          </div>
+          <span class="mb-0"> - UX designer</span>
         </div>
       </hgroup>
 
-      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Situation</h4> 
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Situation</h4>
       <p class="text-indigo-50 mb-8">
-        Our hearing test was already doing very well, around a million people had already tested their hearing with our app.
+        Our hearing test was doing well, around a million people had tested
+        their hearing with our app.
       </p>
-      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Metric</h4> 
-      <p class="text-indigo-50 mb-8">The main metric was completed tests. They're worth measuring because they equate to the core value for the user, but also because they generate data to further improve our algorithms.</p>
-      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Problem</h4> 
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Metric</h4>
       <p class="text-indigo-50 mb-8">
-       Despite the large number of downloads, not everyone started a hearing test. Analyzing the funnel, I discovered that the biggest bottleneck had become the noise meter. This was a feature that we built in to show people it was noisy in their environment.
+        The main metric was completed tests. They're worth measuring because
+        they equate to the core value for the user, but also because they
+        generate data to further improve our algorithms.
       </p>
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">Problem</h4>
+      <p class="text-indigo-50 mb-8">
+        Despite the large number of downloads, not everyone started a hearing
+        test. Analyzing the funnel, I discovered that the biggest bottleneck had
+        become the noise meter. This was a feature that we built in to show
+        people it was noisy in their environment.
+      </p>
+      <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">
+        the funnel
+      </h4>
+      <Funnel :funnelSteps="funnelSteps" :width="600" :height="200" />
     </div>
-    <Hypotheses />
+    <Hypotheses class="w-1/2 px-12" />
     <div class="absolute left-0 bottom-0 w-80 h-80 lg:block hidden">
-      <ProfilePic />
+      <!-- <ProfilePic /> -->
     </div>
   </div>
-  
 </template>
 <script>
 // import { gsap } from "gsap";
 import Hypotheses from "/src/components/hypotheses/Hypotheses.vue";
+import Funnel from "/src/components/Funnel.vue";
 import ProfilePic from "/src/assets/svg/ProfilePic.vue";
 
 export default {
-  components: { Hypotheses, ProfilePic },
+  components: { Hypotheses, ProfilePic, Funnel },
   data() {
     return {
-      }
+      funnelSteps: [
+        { stepId: 1, name: "Open App", percentage: 100 },
+        { stepId: 2, name: "Introduction", percentage: 98 },
+        { stepId: 3, name: "Noise meter", percentage: 84 },
+        { stepId: 4, name: "Set Volume", percentage: 80 },
+        { stepId: 5, name: "Start test", percentage: 79 },
+        { stepId: 6, name: "Finish test", percentage: 76 }
+      ]
+    };
   }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Introduction from "/src/pages/Introduction/index.vue";
 import NoiseMeter from "/src/pages/NoiseMeter/index.vue";
+import Mimi from "/src/pages/Companies/Mimi.vue";
 
-const routes = [{ path: "/", name: "home", component: Introduction }, { path: "/case-study/noise-meter/", name: "noise meter", component: NoiseMeter }];
+const routes = [
+  { path: "/", name: "home", component: Introduction },
+  {
+    path: "/case-study/noise-meter/",
+    name: "noise meter",
+    component: NoiseMeter
+  },
+  {
+    path: "/companies/mimi/",
+    name: "Mimi",
+    component: Mimi
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
