aa8dfed17329a43a3949baf6803fd7133ce2990b
gknook <gknook@gmail.com>
2021-06-28 22:05:33 +0200

    WIP: started building company component

---
+++ src/components/Company.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-indigo-50">
+    <h4 class="text-indigo-50 text-sm uppercase font-bold mb-2">
+      {{ company.name }}
+    </h4>
+    <p>
+      {{ company.description }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  props: ["company"],
+  data() {
+    return {};
+  }
+};
+</script>
