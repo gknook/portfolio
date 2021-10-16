cc1b16970afcc9e8adf79d5f32464f81c977db57
gknook <gknook@gmail.com>
2021-10-16 17:28:07 +0200

    Added a noise illustration (for in the bg)

---
+++ src/components/Illustrations/NoiseIllustration.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="absolute flex z-0 justify-between w-full -bottom-2 pt-12">
+    <div class="flex absolute top-0 right-4">
+      <svg
+        width="14"
+        height="11"
+        viewBox="0 0 14 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M13 3.34783C9.61972 3.93478 6.67887 8.43478 6.40845 10C6.12676 7.3913 4.6507 1.93913 1 1"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </svg>
+      <svg
+        width="38"
+        height="25"
+        viewBox="0 0 38 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="mr-4"
+      >
+        <path
+          d="M36.5 7C26.5 8.5 17.8 20 17 24C16.1667 17.3333 11.8 3.4 1 1"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </svg>
+
+      <svg
+        width="19"
+        height="15"
+        viewBox="0 0 19 15"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M18 4.3913C13.2113 5.23913 9.04507 11.7391 8.66197 14C8.26291 10.2319 6.17183 2.35652 1 1"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </svg>
+    </div>
+    <div class="flex absolute top-4 left-12">
+      <svg
+        width="13"
+        height="11"
+        viewBox="0 0 13 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 6.08696C2.4 6.73913 5.28 8.43478 5.6 10C5.93333 7.3913 7.68 1.93913 12 1"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </svg>
+      <svg
+        width="30"
+        height="25"
+        viewBox="0 0 30 25"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.5 14.5C5 16.1667 12.2 20.5 13 24.5C13.8333 17.8333 18.2 3.9 29 1.5"
+          stroke="black"
+          stroke-width="2"
+          stroke-linecap="round"
+        />
+      </svg>
+    </div>
+
+    <svg
+      width="48"
+      height="142"
+      viewBox="0 0 48 142"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M24.5545 139.835V78.7076M24.5545 34.1149V48.1441M24.5545 48.1441L44.9537 34.1149M24.5545 48.1441V63.1753M24.5545 63.1753L2.16528 46.1399M24.5545 63.1753V78.7076M24.5545 78.7076L46.4463 63.1753M24.5545 26.0982V20.5868"
+        stroke="#1F2937"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        d="M46.5 51.6612C46.5 65.7631 43.8968 78.4574 39.7516 87.57C35.545 96.8176 30.04 101.822 24.5 101.822C18.96 101.822 13.455 96.8176 9.24836 87.57C5.10316 78.4574 2.5 65.7631 2.5 51.6612C2.5 37.5592 5.10316 24.8649 9.24836 15.7523C13.455 6.50469 18.96 1.5 24.5 1.5C30.04 1.5 35.545 6.50469 39.7516 15.7523C43.8968 24.8649 46.5 37.5592 46.5 51.6612Z"
+        stroke="#1F2937"
+        stroke-width="3"
+      />
+    </svg>
+    <svg
+      width="48"
+      height="142"
+      viewBox="0 0 48 142"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M23.4455 139.835V78.7076M23.4455 34.1149V48.1441M23.4455 48.1441L3.04634 34.1149M23.4455 48.1441V63.1753M23.4455 63.1753L45.8347 46.1399M23.4455 63.1753V78.7076M23.4455 78.7076L1.55373 63.1753M23.4455 26.0982V20.5868"
+        stroke="#1F2937"
+        stroke-width="3"
+        stroke-linecap="round"
+      />
+      <path
+        d="M1.5 51.6612C1.5 65.7631 4.10316 78.4574 8.24836 87.57C12.455 96.8176 17.96 101.822 23.5 101.822C29.04 101.822 34.545 96.8176 38.7516 87.57C42.8968 78.4574 45.5 65.7631 45.5 51.6612C45.5 37.5592 42.8968 24.8649 38.7516 15.7523C34.545 6.50469 29.04 1.5 23.5 1.5C17.96 1.5 12.455 6.50469 8.24836 15.7523C4.10316 24.8649 1.5 37.5592 1.5 51.6612Z"
+        stroke="#1F2937"
+        stroke-width="3"
+      />
+    </svg>
+  </div>
+</template>
+<script>
+export default {
+  name: "noise-illustration"
+};
+</script>
