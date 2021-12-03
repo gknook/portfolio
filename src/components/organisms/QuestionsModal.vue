eeeb5d1666f27d6762c7266c29f1422b58b4971c
gknook <gknook@gmail.com>
2021-12-03 22:06:25 +0100

    added role question

--- src/components/organisms/QuestionsModal.vue
+++ src/components/organisms/QuestionsModal.vue
@@ -19,6 +19,49 @@
       <h1 class="text-3xl font-semibold gradient-text mb-8">
         {{ welcomeMessage }}
       </h1>
+
+      <p class="mb-1 text-gray-200 text-left block max-w-sm mx-auto">
+        What role are you hiring for?
+      </p>
+      <ul class="grid grid-cols-2 gap-2 max-w-sm mx-auto">
+        <li
+          v-for="(role, index) in roles"
+          :key="index"
+          class="
+            flex
+            space-x-2
+            items-center
+            bg-gray-900
+            rounded-lg
+            py-4
+            px-4
+            hover:bg-indigo-400
+            transition-all
+            duration-200
+          "
+          :class="[{ 'bg-indigo-500': chosenRole == role.id }]"
+        >
+          <input
+            type="radio"
+            :id="role.id"
+            :name="role.id"
+            :value="role.id"
+            v-model="chosenRole"
+            class="absolute"
+            style="left: -10000px"
+          />
+          <label
+            :for="role.id"
+            class="w-full text-center cursor-pointer font-medium text-purple-50"
+            @click="choseRole(role.id)"
+            ><span class="block w-full text-center text-4xl">{{
+              role.emoji
+            }}</span
+            >{{ role.name }}</label
+          >
+        </li>
+      </ul>
+
       <p class="text-gray-400 text-xs max-w-sm mx-auto" v-if="userName != null">
         You indicated you're looking for a product designer.
       </p>
@@ -26,111 +69,90 @@
         And that you're interested in [[Data]] [[User Research]] and [[Visual
         Design]].
       </p>
-      <label for="name" class="mb-4 block text-gray-200" v-if="userName == null"
-        >What's your name?</label
-      >
-      <div class="max-w-sm mx-auto relative">
-        <div
-          id="keyboard-hint"
-          class="
-            absolute
-            bg-gray-800
-            rounded-md
-            text-gray-500 text-base
-            px-2
-            py-1
-            right-0
-            top-0
-            mt-2
-            mr-2
-            opacity-0
-          "
+      <InputFieldWithKeyboard
+        ref="inputField"
+        v-if="userName == null"
+        @pressed-enter="setUserName"
+        :inputName="'firstname'"
+        :inputType="'text'"
+        :keyboardHint="'enter'"
+        :placeholder="'Type your name and press enter'"
+      />
+      <div class="max-w-sm mx-auto flex flex-col space-y-4">
+        <GlowyButton :cta="buttonCopy" @clicked-button="setUserName" />
+        <p
+          class="text-gray-400 text-xs max-w-sm mx-auto"
+          v-if="userName == null"
         >
-          enter
-        </div>
-        <div class="flex flex-col space-y-4 mb-2">
-          <input
-            type="text"
-            id="name"
-            v-model="name"
-            name="name"
-            placeholder="Type your name and press enter"
-            class="bg-gray-900 px-4 py-3 rounded-xl w-full font-semibold"
-            v-if="userName == null"
-          />
-          <button
-            class="
-              font-semibold
-              text-blue-100
-              rounded-xl
-              bg-gradient-to-br
-              from-indigo-700
-              via-blue-600
-              to-green-600
-              px-8
-              py-3
-            "
-            @click="setUserName"
-          >
-            {{ buttonCopy }}
-          </button>
-        </div>
+          I'm only storing this in your LocalStorage. Meaning that I'm just
+          using it to personalize during your visits. Your name won't leave your
+          browser.
+        </p>
       </div>
-      <p class="text-gray-400 text-xs max-w-sm mx-auto" v-if="userName == null">
-        I'm only storing this in your LocalStorage. Meaning that I'm just using
-        it to personalize your experience temporarily.
-      </p>
     </div>
+    <GlowyButton :cta="'clear storage'" @clicked-button="clearStorage" />
   </div>
 </template>
 <script>
-import { gsap } from "gsap";
+// import { gsap } from "gsap";
 import { mapGetters, mapMutations } from "vuex";
+import GlowyButton from "/src/components/atoms/GlowyButton.vue";
+import InputFieldWithKeyboard from "/src/components/organisms/InputFieldWithKeyboard.vue";
 export default {
   name: "questions-modal",
+  components: { GlowyButton, InputFieldWithKeyboard },
   data() {
     return {
-      name: "",
-      keyBoardTimeline: null,
-      welcomeMessage: "Welcome to my portfolio"
+      roles: [
+        { category: "design", emoji: "👨🏻‍🎨", name: "Product Designer", id: "pd" },
+        { category: "design", emoji: "👨🏻‍🎨", name: "Game Designer", id: "gd" },
+        { category: "product", emoji: "👨‍💼", name: "Product Manager", id: "pm" },
+        {
+          category: "product",
+          emoji: "👨‍💼",
+          name: "Head of Product",
+          id: "hop"
+        },
+        { category: "dev", emoji: "👨🏻‍💻", name: "Front-end Developer", id: "fe" },
+        {
+          category: "none",
+          emoji: "🧙🏻‍♂️",
+          name: "None, just browsing",
+          id: "none"
+        }
+      ],
+      chosenRole: null
     };
   },
-  mounted() {
-    this.initKeyBoardTimeline();
-    this.initModalCopy();
-  },
+  mounted() {},
   computed: {
-    ...mapGetters(["userName"])
+    ...mapGetters(["userName"]),
+    welcomeMessage() {
+      if (this.userName) {
+        return `Welcome back ${this.userName}`;
+      } else {
+        return "Welcome to my portfolio!";
+      }
+    },
+    buttonCopy() {
+      if (this.userName) {
+        return "re-enter";
+      } else {
+        return "let's go!";
+      }
+    }
   },
   methods: {
     ...mapMutations(["updateUserName"]),
-    initKeyBoardTimeline() {
-      console.log("start timeline");
-      this.keyBoardTimeline = gsap
-        .timeline()
-        .pause()
-        .to("#keyboard-hint", { opacity: 1, duration: 0.2 });
-    },
-    initModalCopy() {
-      if (this.userName == null) {
-        this.welcomeMessage = "Welcome to my portfolio!";
-        this.buttonCopy = "let's go";
-      } else {
-        this.welcomeMessage = `Welcome back ${this.userName}`;
-        this.buttonCopy = "re-enter";
-      }
-    },
     setUserName() {
-      this.updateUserName(this.name);
-    }
-  },
-  watch: {
-    name: function () {
-      if (this.name.length == 1) {
-        this.keyBoardTimeline.play();
-      } else if (this.name.length == 0) {
-        this.keyBoardTimeline.reverse();
-      }
+      this.updateUserName(this.$refs.inputField.name);
+    },
+    clearStorage() {
+      localStorage.clear();
+      this.$emit("rerender");
+    },
+    choseRole(role) {
+      console.log(role);
     }
   }
 };
