62e2fdd2188523ee158835abdc8c8f19a75786ed
gknook <gknook@gmail.com>
2021-12-03 22:05:55 +0100

    added a button-glow

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
         "card-glow":
           "-2px -2px 10px 0px rgba(249, 168, 212,0.7), 0px 2px 10px 0px rgba(139, 92, 246, 0.6), 2px -2px 10px 0px rgba(252, 211, 77, 0.7)",
         "card-glow-grow":
-          "-8px -8px 40px 0px rgba(249, 168, 212,0.7), 0px 8px 40px 0px rgba(139, 92, 246, 0.6), 8px -8px 40px 0px rgba(252, 211, 77, 0.7)"
+          "-8px -8px 40px 0px rgba(249, 168, 212,0.7), 0px 8px 40px 0px rgba(139, 92, 246, 0.6), 8px -8px 40px 0px rgba(252, 211, 77, 0.7)",
+        "button-glow": "0px 0px 32px 2px rgba(86, 233, 253, 0.7)"
         /* in order: x offset, y offset, blur size, spread size, color */
         /* blur size and spread size are optional (they default to 0) */
       }
