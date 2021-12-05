3f596fc9f715100e6b628aca25a8e1126b11e97e
gknook <gknook@gmail.com>
2021-12-05 20:57:16 +0100

    create principles component

---
+++ src/components/organisms/Principle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul class="max-w-screen-md mx-auto md:px-12 sm:px-6">
+      <li
+        v-for="(principle, index) in workPrinciples"
+        :key="index"
+        class="mb-12 text-gray-100"
+      >
+        <h2 class="text-5xl font-semibold mb-4">{{ principle.name }}</h2>
+        <p class="text-lg mb-4">{{ principle.principle }}</p>
+        <div class="p-4 pb-8 bg-gray-900 -mx-4">
+          <h3 class="font-semibold mb-2">Example of how I demonstrated this</h3>
+          <p class="italic">{{ principle.example }}</p>
+        </div>
+        <ul class="flex space-x-4 -mt-4">
+          <li
+            class="bg-gray-700 px-2 py-1 rounded-md"
+            v-for="(skill, index) in principle.skills"
+            :key="index"
+          >
+            {{ skill }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import workPrinciples from "/src/data/workPrinciple.js";
+export default {
+  data() {
+    return {
+      workPrinciples: workPrinciples
+    };
+  }
+};
+</script>
---
+++ src/data/workPrinciple.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    name: "Product impact",
+    principle:
+      "Not only is it important to deliver high quality products, I seek to improve the company's trajectory through the products I work on.",
+    example:
+      "Creating [[Mimi's sound processing prototype]] probably did more for Mimi in terms of convincing clients than white papers, product demos and pitches combined. Instead of having to imagine how much of a benefit people with hearing loss would have from the product, we were able to show our (well-hearing) potential clients.",
+    skills: ["shipping", "big picture thinking", "high quality"]
+  },
+  {
+    name: "Decision mix",
+    principle:
+      "I combine data with qualitative inputs and instinct to make decisions. The center of gravity within that mix is dependent on the situation (e.g. speed, reversibility of decision, etc.).",
+    example:
+      "Analyzing funnel data showed me the need to improve the noise meter, while qualitative inputs—like doing user research, having a deep understanding of hearing and discussing with the research team—informed what direction the solution should take.",
+    skills: [
+      "connecting the dots",
+      "data",
+      "data-driven",
+      "gut feel",
+      "user research"
+    ]
+  },
+  {
+    name: "Learing process",
+    principle:
+      "I bootstrap my learning process to become effective quickly. I read books and papers on the topic, I talk to domain experts, I visualize my current understanding and I try things out.",
+    example:
+      "To learn how hearing (loss) works, I talked to hearing scientists, industry experts and people with hearing loss. I read papers on hearing loss, and testing procedures. I visualized how hearing loss works, and created a graph to explain our processing. Then discussed those visualizations with our researchers to validate the understanding.",
+    skills: ["learning", "expertise"]
+  },
+  {
+    name: "User research",
+    principle:
+      "I talk to users not just to learn whether they're able to use my product. To understand their context, their needs, what excites and motivates them and why they're looking for a solution.",
+    example:
+      "For Atium, before we even wrote one line of code or designed one screen, first I interviewed team leads about how they managed their teams remotely. How they built connection and what they struggled with.",
+    skills: ["user research", "usability testing"]
+  },
+  {
+    name: "Team work",
+    principle:
+      "I leverage every team member's inner creative to build great products—designers, developers and researchers alike. Creative problem solving isn't the purview of one or two roles, it's the job of the whole team.",
+    example:
+      "At Atium all three of us (founders) created pitches for both problems to solve and solutions to those problems. Core to the process was going through these pitches together and improving the solution before implementation. Similarly at Mimi I regularly facilitated product vision workshops to get input from the whole team (even company) on where the product should go.",
+    skills: ["creative problem solving", "facilitation", "product vision"]
+  },
+  {
+    name: "Complete experience",
+    principle:
+      "I look at the customer's experience broadly. The product is more than just the product. It starts when the customer first learns about the product and includes their experience with the ads, the landing page, the onboarding process, but also customer support, the App Store/Play Store presence and in a B2B context sales meetings and trade show visits.",
+    example:
+      "When developing Mimi's SDKs we built a customer journey map of the whole experience and defined what the customers state of mind was, what decisions they needed to take and most importantly how we could help them. The product experience itself was only a minor part of this whole experience and this process initiated the creation of sales enablement material from brochures to integrate.mimi.io, as well as initiating the idea to create a sales enablement product team (that eventually created [[Mimi's sound processing prototype]]).",
+    skills: ["product marketing", "customer experience", "journey mapping"]
+  }
+];
